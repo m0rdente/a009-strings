5fbbf07f48c1eb0db6b0c575782ccfa1a3bf9f0e
Priscila Mordente <priscilamordente@gmail.com>
2022-07-26 20:49:08 -0300

    feito e atualizado exercicio 009

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,5 +1,6 @@
-const frase = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas NÃO DEIXE O GATO sair"'
+const frase = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"'
 const novaCor = frase.replace("verde","rosa")
-const segundaCor = novaCor.replace ("azul","laranja")
+const segundaCor = novaCor.replace("azul","laranja")
+const fraseFinal = segundaCor.replaceAll("não deixe o gato sair", "NÃO DEIXE O GATO SAIR")
 
-console.log (segundaCor)
+console.log (fraseFinal)
